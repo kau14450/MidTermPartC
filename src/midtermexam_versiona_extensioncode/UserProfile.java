a9a6830d21601258aacf6eb961c29373984053eb
Satnamjeet Kaur <134073832+kau14450@users.noreply.github.com>
2023-06-23 10:28:16 -0400

    Last commit for fetch and merge

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -12,6 +12,7 @@ package midtermexam_versiona_extensioncode;
  * Students are reminded to add themselves as modifiers when editing the
  * code.
  * @author Satnamjeet Kaur, 2023
+ Editing for last (Fetch and merge step).
  */
 public class UserProfile 
 {
